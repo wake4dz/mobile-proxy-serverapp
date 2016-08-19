beef7c608362e9ee1e3c9ae82f961403f68afc14
Zachary Puster <zachary.puster@rosetta.com>
2016-08-19 16:08:23 -0400

    WIP - circular categories. Currently saying that @path is a '/' when the entire path is being passes

--- src/main/java/com/wakefern/Circular/CircularCategory.java
+++ src/main/java/com/wakefern/Circular/CircularCategory.java
@@ -7,7 +7,10 @@ import com.wakefern.global.ServiceMappings;
 import com.wakefern.mywebgrocer.models.MWGHeader;
 import com.wakefern.request.HTTPRequest;
 
-import javax.ws.rs.*;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import java.io.IOException;
 
 /**
@@ -15,31 +18,34 @@ import java.io.IOException;
  */
 
 @Path(ApplicationConstants.Requests.Circular.Categories)
-public class CircularCategories extends BaseService{
-    @GET
+public class CircularCategory extends BaseService{
     @Produces("application/*")
-    @Path("/{chainId}/stores/{storeId}/categories")
-    public String getInfo(@PathParam("chainId") String chainId, @PathParam("storeId") String storeId,
-                          @HeaderParam("Authorization") String authToken) throws Exception, IOException {
+    @Path("/{chainId}/stores/{storeId}/categories/{categoryId}")
+    public String getInfo(@PathParam("chainId") String chainId, @PathParam("storeId") String storeId, @PathParam("categoryId") String categoryId,
+                          @HeaderParam("Authorization") String authToken)throws Exception, IOException {
+
         JSONObject myJSONObj = new JSONObject();
 
         this.token = authToken;
-        //this.path = ApplicationConstants.Requests.Circular.Categories+ "/" + chainId + "/stores/" + storeId + "/categories";
         this.path = ApplicationConstants.Requests.Circular.Categories + ApplicationConstants.StringConstants.backSlash
                 + chainId + ApplicationConstants.StringConstants.stores + ApplicationConstants.StringConstants.backSlash
-                + storeId + ApplicationConstants.StringConstants.categories;
+                + storeId + ApplicationConstants.StringConstants.categories + ApplicationConstants.StringConstants.backSlash
+                + categoryId;
+
+//        this.path = ApplicationConstants.Requests.Circular.Categories + "/" + chainId + "/stores/" + storeId + "/categories/" + categoryId;
+
+
 
         ServiceMappings secondMapping = new ServiceMappings();
         secondMapping.setMapping(this);
 
-
-        myJSONObj.put(ApplicationConstants.RequestType.HTTPRequest + ApplicationConstants.RequestType.CircularCategories,
-                HTTPRequest.executeGet( secondMapping.getPath(), secondMapping.getgenericHeader()));
+        myJSONObj.put("HTTPRequest: Circular Categories", HTTPRequest.executeGet( secondMapping.getPath(),
+                secondMapping.getgenericHeader()));
 
         return myJSONObj.toString();
     }
 
-    public CircularCategories(){
+    public CircularCategory(){
         this.serviceType = new MWGHeader();
     }
 }

4e7a3cda203ee92e5da8b3ea7783536da578c8b6
Zachary Puster <zachary.puster@rosetta.com>
2016-08-26 15:00:04 -0400

    Products: Products with in category, special products with in category, products by search

---
+++ src/main/java/com/wakefern/Products/ProductBySearch.java
@@ -0,0 +1,38 @@
+package com.wakefern.Products;
+
+import com.ibm.json.java.JSONObject;
+import com.wakefern.global.ApplicationConstants;
+import com.wakefern.global.BaseService;
+import com.wakefern.global.ServiceMappings;
+import com.wakefern.mywebgrocer.models.MWGHeader;
+import com.wakefern.request.HTTPRequest;
+
+import javax.ws.rs.*;
+import java.io.IOException;
+
+/**
+ * Created by zacpuste on 8/25/16.
+ */
+@Path(ApplicationConstants.Requests.Categories.ProductStore)
+public class ProductBySearch extends BaseService {
+    @GET
+    @Produces("application/*")
+    @Path("/{storeId}/search")
+    public String getInfo(@PathParam("storeId") String storeId,
+                          @HeaderParam("Authorization") String authToken) throws Exception, IOException {
+        JSONObject myJSONObj = new JSONObject();
+
+        this.token = authToken;
+        this.path = ApplicationConstants.Requests.Categories.ProductStore
+                + ApplicationConstants.StringConstants.backSlash + storeId + ApplicationConstants.StringConstants.search;
+
+        ServiceMappings secondMapping = new ServiceMappings();
+        secondMapping.setMapping(this);
+
+        return HTTPRequest.executeGet(secondMapping.getPath(), secondMapping.getgenericHeader());
+
+    }
+    public ProductBySearch(){
+        this.serviceType = new MWGHeader();
+    }
+}

ead7a15ac3f4ca78b8afb8ecc9853497a11ca009
Zachary Puster <zachary.puster@rosetta.com>
2016-09-30 16:58:11 -0400

    PreferredStore

--- src/main/java/com/wakefern/Recipes/UpdateProfile.java
+++ src/main/java/com/wakefern/Recipes/UpdateProfile.java
@@ -8,6 +8,8 @@ import com.wakefern.request.HTTPRequest;
 
 import javax.ws.rs.*;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by zacpuste on 9/27/16.
@@ -17,9 +19,17 @@ public class UpdateProfile extends BaseService {
     @PUT
     @Produces("application/*")
     @Path("/{userId}")
-    public String getInfo(@PathParam("userId") String userId, @QueryParam("email") String email,
-                          @HeaderParam("Authorization") String authToken, String jsonBody) throws Exception, IOException {
+    public String getInfo(@PathParam("userId") String userId, @QueryParam("email") String email, @QueryParam("chainId") String chainId, @QueryParam("storeId") String storeId,
+                          @HeaderParam("Authorization") String authToken) throws Exception, IOException {
         this.token = authToken;
+
+        GetProfile getProfile = new GetProfile();
+        String jsonBody = getProfile.getInfo(userId, chainId, email, authToken);
+        Pattern pattern = Pattern.compile("(<PreferredStore><Id>.*</Id></PreferredStore>)");
+        Matcher matcher = pattern.matcher(jsonBody);
+        jsonBody = matcher.replaceAll("<PreferredStore><Id>" + storeId + "</Id></PreferredStore>");
+        jsonBody = jsonBody.replaceAll("<?xml version=\"1.0\" encoding=\"UTF-8\"?>","");
+        jsonBody = jsonBody.replaceAll("xmlns=\"http://schemas.datacontract.org/2004/07/MyWebGrocer.Account.Service.ServiceModels\"", "");
 
         this.path = ApplicationConstants.Requests.Recipes.UpdateProfile
                 + ApplicationConstants.StringConstants.backSlash + userId + ApplicationConstants.StringConstants.emailParam + email;

33a9e26c8071c106e2a1704514b7f4a50e18df11
Zachary Puster <zachary.puster@rosetta.com>
2016-10-28 12:17:06 -0400

    Shopping List Item Note

---
+++ src/main/java/com/wakefern/ShoppingLists/ShoppingListItemNote.java
@@ -0,0 +1,77 @@
+package com.wakefern.ShoppingLists;
+
+/**
+ * Created by zacpuste on 10/28/16.
+ */
+
+import com.wakefern.global.ApplicationConstants;
+import com.wakefern.global.BaseService;
+import com.wakefern.global.ServiceMappings;
+import com.wakefern.mywebgrocer.models.MWGHeader;
+import com.wakefern.request.HTTPRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zacpuste on 8/25/16.
+ */
+@Path(ApplicationConstants.Requests.ShoppingLists.slChains)
+public class ShoppingListItemNote extends BaseService {
+    @PUT
+    @Produces("application/*")
+    /**
+     * JSON for put:
+     * {"Note":1477667596890}
+     */
+    @Path("/{chainId}/users/{userId}/lists/{listId}/items/{itemId}")
+    public Response getInfoResponse(@PathParam("chainId") String chainId, @PathParam("userId") String userId, @PathParam("listId") String listId,
+                                    @PathParam("itemId") String itemId, @DefaultValue("")@QueryParam("isMember") String isMember,
+                                    @HeaderParam("Authorization") String authToken, String jsonBody) throws Exception, IOException {
+        prepareResponse(chainId, userId, listId, itemId, isMember, authToken);
+        Map<String, String> map = new HashMap<>();
+        String path = ApplicationConstants.Requests.baseURLV5 + this.path;
+        map.put(ApplicationConstants.Requests.Header.contentAuthorization, authToken);
+        map.put(ApplicationConstants.Requests.Header.contentType, "application/vnd.mywebgrocer.list-item+json");
+
+        try {
+            return this.createValidResponse(HTTPRequest.executePut("", path, "", jsonBody, map));
+        } catch (Exception e){
+            return this.createErrorResponse(e);
+        }
+    }
+
+    public String getInfo(String chainId, String userId, String listId, String itemId, String authToken, String isMember, String jsonBody) throws Exception, IOException {
+        prepareResponse(chainId, userId, listId, itemId, isMember, authToken);
+
+        Map<String, String> map = new HashMap<>();
+        String path = ApplicationConstants.Requests.baseURLV5 + this.path;
+        map.put(ApplicationConstants.Requests.Header.contentAuthorization, authToken);
+        map.put(ApplicationConstants.Requests.Header.contentType, "application/vnd.mywebgrocer.list-item+json");
+
+        return HTTPRequest.executePut("", path, "", jsonBody, map);
+    }
+
+    public ShoppingListItemNote(){
+        this.serviceType = new MWGHeader();
+    }
+
+    private void prepareResponse(String chainId, String userId, String listId, String itemId, String isMember, String authToken){
+        this.token = authToken;
+        this.path = ApplicationConstants.Requests.ShoppingLists.slChains
+                + ApplicationConstants.StringConstants.backSlash + chainId + ApplicationConstants.StringConstants.users
+                + ApplicationConstants.StringConstants.backSlash + userId + ApplicationConstants.StringConstants.lists
+                + ApplicationConstants.StringConstants.backSlash + listId + ApplicationConstants.StringConstants.items
+                + ApplicationConstants.StringConstants.backSlash + itemId;
+        if(!isMember.isEmpty()){
+            this.path = ApplicationConstants.Requests.ShoppingLists.slChains
+                    + ApplicationConstants.StringConstants.backSlash + chainId + ApplicationConstants.StringConstants.users
+                    + ApplicationConstants.StringConstants.backSlash + userId + ApplicationConstants.StringConstants.lists
+                    + ApplicationConstants.StringConstants.backSlash + listId + ApplicationConstants.StringConstants.items
+                    + ApplicationConstants.StringConstants.backSlash + itemId + ApplicationConstants.StringConstants.isMember;
+        }
+    }
+}

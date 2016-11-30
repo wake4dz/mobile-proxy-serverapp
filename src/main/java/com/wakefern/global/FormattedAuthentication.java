9bddcd0f0ee166ef4f6e9a51ff89e5873d8e1075
Zachary Puster <zachary.puster@rosetta.com>
2016-11-30 13:01:00 -0500

    changed substring position

--- src/main/java/com/wakefern/global/FormattedAuthentication.java
+++ src/main/java/com/wakefern/global/FormattedAuthentication.java
@@ -115,8 +115,7 @@ public class FormattedAuthentication {
             else if(serviceName.equals(ApplicationConstants.FormattedAuthentication.S2GSisterStore)){
                 String uri = currentService.get(ApplicationConstants.FormattedAuthentication.Uri).toString();
                 String[] query = uri.split("strid=");
-                String id = query[1].substring(5, query[1].length());
-
+                String id = query[1].substring(4, query[1].length());
                 try {
                     StoreDetails storeDetails = new StoreDetails();
                     String storesJson = storeDetails.getInfo(chainId, id, planningAuth);

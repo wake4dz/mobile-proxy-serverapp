5dd07ac64e3ee90ab00831aaa39cd1d148334121
Bob <bob@labelinteractive.com>
2017-12-26 11:43:59 -0500

    Added new Path Params to ServiceMappings.

--- src/main/java/com/wakefern/global/ServiceMappings.java
+++ src/main/java/com/wakefern/global/ServiceMappings.java
@@ -202,6 +202,14 @@ public class ServiceMappings {
 			if (serviceObj.requestParams.containsKey(MWGApplicationConstants.Requests.Params.Path.productSKU)) {
 				path = replacePathParam(MWGApplicationConstants.Requests.Params.Path.productSKU, path, serviceObj);
 			}
+			
+			if (serviceObj.requestParams.containsKey(MWGApplicationConstants.Requests.Params.Path.itemID)) {
+				path = replacePathParam(MWGApplicationConstants.Requests.Params.Path.itemID, path, serviceObj);
+			}
+			
+			if (serviceObj.requestParams.containsKey(MWGApplicationConstants.Requests.Params.Path.circularID)) {
+				path = replacePathParam(MWGApplicationConstants.Requests.Params.Path.circularID, path, serviceObj);
+			}
 		}
 		
 		// Build the query string, if there are any query parameters

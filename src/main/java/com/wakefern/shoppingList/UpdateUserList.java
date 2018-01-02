08d17e3fa5efee467f189127b6030965cbac00d0
Bob <bob@labelinteractive.com>
2018-01-02 17:03:48 -0500

    Added endpoint:  Update an existing User's List.

---
+++ src/main/java/com/wakefern/shoppingList/UpdateUserList.java
@@ -0,0 +1,64 @@
+package com.wakefern.shoppingList;
+
+import com.wakefern.global.ApplicationConstants;
+import com.wakefern.global.BaseService;
+import com.wakefern.mywebgrocer.models.MWGHeader;
+import com.wakefern.mywebgrocer.MWGApplicationConstants;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.HashMap;
+
+@Path(MWGApplicationConstants.Requests.ShoppingList.prefix)
+public class UpdateUserList extends BaseService {
+	
+	//-------------------------------------------------------------------------
+	// Public Methods
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 */
+    public UpdateUserList() {
+        this.requestPath = MWGApplicationConstants.Requests.ShoppingList.prefix + MWGApplicationConstants.Requests.ShoppingList.list;
+    }
+    
+	@PUT
+    @Consumes(MWGApplicationConstants.Headers.ShoppingList.email)
+    @Produces(MWGApplicationConstants.Headers.generic)
+    @Path(MWGApplicationConstants.Requests.ShoppingList.list)
+    public Response getResponse(
+    		@PathParam(MWGApplicationConstants.Requests.Params.Path.chainID) String chainID,
+    		@PathParam(MWGApplicationConstants.Requests.Params.Path.userID) String userID,
+    		@PathParam(MWGApplicationConstants.Requests.Params.Path.listID) String listID,
+    		
+    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.storeID) String storeID,
+    		
+    		@HeaderParam(MWGApplicationConstants.Headers.Params.auth) String sessionToken,
+    		
+    		String jsonData
+	) throws Exception, IOException {
+        		
+		this.requestHeader = new MWGHeader(ApplicationConstants.jsonHeaderType, MWGApplicationConstants.Headers.ShoppingList.email, sessionToken);
+		this.requestParams = new HashMap<String, String>();
+		this.queryParams   = new HashMap<String, String>();
+		
+		// Build the Map of Request Path parameters
+		this.requestParams.put(MWGApplicationConstants.Requests.Params.Path.chainID, chainID);
+		this.requestParams.put(MWGApplicationConstants.Requests.Params.Path.userID, userID);
+		this.requestParams.put(MWGApplicationConstants.Requests.Params.Path.listID, listID);
+		
+		// Build the Map of Query String parameters.
+		this.queryParams.put(MWGApplicationConstants.Requests.Params.Query.storeID, storeID);
+
+        try {
+            String jsonResponse = this.mwgRequest(BaseService.ReqType.PUT, jsonData);
+            return this.createValidResponse(jsonResponse);
+        
+        } catch (Exception e) {
+            return this.createErrorResponse(e);
+        }
+    }
+}
+

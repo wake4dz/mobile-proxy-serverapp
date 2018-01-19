7b3d87bb1c5c59a416d27e94bf534d0307536816
Bob <bob@labelinteractive.com>
2018-01-19 16:29:05 -0500

    Code reformatting only.

--- src/main/java/com/wakefern/coupons/Coupons.java
+++ src/main/java/com/wakefern/coupons/Coupons.java
@@ -30,175 +30,203 @@ import java.util.logging.Logger;
 
 @Path(ApplicationConstants.Requests.Coupons.GetCoupons)
 public class Coupons extends BaseService {
-    public JSONObject matchedObjects;
+	public JSONObject matchedObjects;
 	private long startTime, endTime;
 	private final static Logger logger = Logger.getLogger("Coupons");
 
-    @GET
-    @Produces(MWGApplicationConstants.Headers.generic)
-    public Response getInfoResponse(@DefaultValue(WakefernApplicationConstants.Requests.Coupons.Metadata.PPC_All)
-                                    @QueryParam(WakefernApplicationConstants.Requests.Coupons.Metadata.PPC) String ppcParam,
-                                    @DefaultValue("") @QueryParam("query") String query,
-                                    @HeaderParam("Authorization") String authToken) throws Exception, IOException {
-    	
-    		this.requestToken = ApplicationConstants.Requests.Tokens.couponToken;
-        
-        startTime = System.currentTimeMillis();
-        matchedObjects = new JSONObject();
-        this.requestPath = ApplicationConstants.Requests.Coupons.BaseCouponURL + ApplicationConstants.Requests.Coupons.GetCoupons
-                    + WakefernApplicationConstants.Requests.Coupons.Metadata.PPCQuery + ppcParam;
+	@GET
+	@Produces(MWGApplicationConstants.Headers.generic)
+	public Response getInfoResponse(
+			@DefaultValue(WakefernApplicationConstants.Requests.Coupons.Metadata.PPC_All) 
+			@QueryParam(WakefernApplicationConstants.Requests.Coupons.Metadata.PPC) String ppcParam,
+			
+			@DefaultValue("") 
+			@QueryParam("query") String query, 
+			
+			@HeaderParam("Authorization") String authToken
+		) throws Exception, IOException {
 
-        //Execute Post
-        ServiceMappings serviceMappings = new ServiceMappings();
-        serviceMappings.setCouponMapping(this);
+		this.requestToken = ApplicationConstants.Requests.Tokens.couponToken;
 
-        try {
-            String coupons = HTTPRequest.executePostJSON(serviceMappings.getPath(), "", serviceMappings.getgenericHeader(), 0);
-            
-            endTime = System.currentTimeMillis();
-        	System.out.println("[Coupons]::Total process time (ms): "+ (endTime - startTime));
-        	
-        	ObjectMapper mapper = new ObjectMapper();
-        	CouponDAO[] couponDaoArr = mapper.readValue(coupons, CouponDAO[].class);
+		startTime = System.currentTimeMillis();
+		matchedObjects = new JSONObject();
+		
+		this.requestPath = ApplicationConstants.Requests.Coupons.BaseCouponURL
+				+ ApplicationConstants.Requests.Coupons.GetCoupons
+				+ WakefernApplicationConstants.Requests.Coupons.Metadata.PPCQuery + ppcParam;
 
-            if (query.isEmpty()){// == "") {
+		// Execute Post
+		ServiceMappings serviceMappings = new ServiceMappings();
+		serviceMappings.setCouponMapping(this);
 
-            	String[] couponOmittedValuesArr = {
-            		    "coupon_id",
-            		    "requirement_description",
-            		    "brand_name",
-            		    "short_description",
-            		    "image_url",
-            		    "total_downloads", 
-            		    "external_id",
-            		    "pos_live_date",
-            		    "display_start_date",
-            		    "display_end_date",
-            		    "Category",
-            		    "requirement_upcs",
-            		    "expiration_date",
-            		    "coupon_value",
-            		    "targeting_buckets"
-            	};
-//            			"reward_upcs", "featured", "id", "__createdAt", "__updatedAt", "__version", "__deleted", "total_downloads", 
-//            		    "targeted_offer", "tags", "enabled", "offer_type", "long_description_header"};
-            	
-            	FilterProvider filterCouponFields = new SimpleFilterProvider()
-            			.addFilter("filterByValue", SimpleBeanPropertyFilter.filterOutAllExcept(couponOmittedValuesArr));
-            	ObjectWriter writer = mapper.writer(filterCouponFields);
-            	for(CouponDAO couponDao : couponDaoArr){
-            		try{
-            			//"short_description": "Save $.50 On Crystal Farms Chunk Cheese"
-	            		String [] shortDesc = couponDao.getShortDescription().split(" ");
-            			if(shortDesc[1].contains("$"))
-            				couponDao.setCouponValue(shortDesc[1]);
-            			else if(shortDesc[0].contains("$")){
-            				couponDao.setCouponValue(shortDesc[0]);
-            			} else{
-            				for(String value : shortDesc){
-            					if(value.contains("$")){
-                    				couponDao.setCouponValue(value);
-                    				break;
-            					}
-            				}
-            			}
-            		} catch(Exception e){
-                    	logger.log(Level.SEVERE, "[getInfoResponse]::Exception retrieved coupon ppc: {0}, msg: {1}", new Object[]{ppcParam, e.toString()});
-            		}
-            	}
-            	
-            	String cnrDaoStr = writer.writeValueAsString(couponDaoArr);
-            	
-                return this.createValidResponse(cnrDaoStr);
-            }
+		try {
+			String coupons = HTTPRequest.executePostJSON(serviceMappings.getPath(), "", serviceMappings.getgenericHeader(), 0);
 
-            JSONArray matchedObjects2 = new JSONArray();
-            return this.createValidResponse(search(coupons, query, matchedObjects2).toString());
-        } catch (Exception e){
-            return this.createErrorResponse(e);
-        }
-    }
+			endTime = System.currentTimeMillis();
+			System.out.println("[Coupons]::Total process time (ms): " + (endTime - startTime));
 
-    public String getInfo(String ppcParam,  String query, String authToken) throws Exception, IOException {
-        
-    		this.requestToken = ApplicationConstants.Requests.Tokens.couponToken;
+			ObjectMapper mapper = new ObjectMapper();
+			CouponDAO[] couponDaoArr = mapper.readValue(coupons, CouponDAO[].class);
 
-        matchedObjects = new JSONObject();
-        this.requestPath = ApplicationConstants.Requests.Coupons.BaseCouponURL + ApplicationConstants.Requests.Coupons.GetCoupons
-                + WakefernApplicationConstants.Requests.Coupons.Metadata.PPCQuery + ppcParam;
+			if (query.isEmpty()) {
 
-        //Execute Post
-        ServiceMappings serviceMappings = new ServiceMappings();
-        serviceMappings.setCouponMapping(this);
+				String[] couponOmittedValuesArr = { 
+						"coupon_id", 
+						"requirement_description", 
+						"brand_name",
+						"short_description", 
+						"image_url", 
+						"total_downloads", 
+						"external_id", 
+						"pos_live_date",
+						"display_start_date", 
+						"display_end_date", 
+						"Category", 
+						"requirement_upcs", 
+						"expiration_date",
+						"coupon_value", 
+						"targeting_buckets" 
+				};
 
-        String coupons = HTTPRequest.executePostJSON(serviceMappings.getPath(), "", serviceMappings.getgenericHeader(), 0);
+				FilterProvider filterCouponFields = new SimpleFilterProvider().addFilter("filterByValue", SimpleBeanPropertyFilter.filterOutAllExcept(couponOmittedValuesArr));
+				
+				ObjectWriter writer = mapper.writer(filterCouponFields);
+				
+				for (CouponDAO couponDao : couponDaoArr) {
+					try {
+						// "short_description": "Save $.50 On Crystal Farms Chunk Cheese"
+						String[] shortDesc = couponDao.getShortDescription().split(" ");
+						
+						if (shortDesc[1].contains("$"))
+							couponDao.setCouponValue(shortDesc[1]);
+						
+						else if (shortDesc[0].contains("$")) {
+							couponDao.setCouponValue(shortDesc[0]);
+						
+						} else {
+							for (String value : shortDesc) {
+								if (value.contains("$")) {
+									couponDao.setCouponValue(value);
+									break;
+								}
+							}
+						}
+					
+					} catch (Exception e) {
+						logger.log(Level.SEVERE, "[getInfoResponse]::Exception retrieved coupon ppc: {0}, msg: {1}",
+								new Object[] { ppcParam, e.toString() });
+					}
+				}
 
-        if (query.isEmpty()){// == "") {
-            return coupons;
-        }
+				String cnrDaoStr = writer.writeValueAsString(couponDaoArr);
 
-        JSONArray matchedObjects2 = new JSONArray();
-        return search(coupons, query, matchedObjects2).toString();
-    }
+				return this.createValidResponse(cnrDaoStr);
+			}
 
-    public Coupons () {this.requestHeader = new WakefernHeader();}
+			JSONArray matchedObjects2 = new JSONArray();
+			return this.createValidResponse(search(coupons, query, matchedObjects2).toString());
+		
+		} catch (Exception e) {
+			return this.createErrorResponse(e);
+		}
+	}
 
-    private JSONObject search(String coupons, String query, JSONArray matchedObjects2){
-        query = query.toLowerCase();
+	public String getInfo(String ppcParam, String query, String authToken) throws Exception, IOException {
 
-        JSONArray jsonArray = new JSONArray(coupons);
+		this.requestToken = ApplicationConstants.Requests.Tokens.couponToken;
 
-        for(Object coupon: jsonArray){
-            JSONObject currentCoupon = (JSONObject) coupon;
+		matchedObjects = new JSONObject();
+		
+		this.requestPath = ApplicationConstants.Requests.Coupons.BaseCouponURL
+				+ ApplicationConstants.Requests.Coupons.GetCoupons
+				+ WakefernApplicationConstants.Requests.Coupons.Metadata.PPCQuery + ppcParam;
 
-            String brandName = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.brandName);
-            String shortDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.shortDescription);
-            String longDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.longDescription);
-            String requirementDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.requirementDescription);
+		// Execute Post
+		ServiceMappings serviceMappings = new ServiceMappings();
+		serviceMappings.setCouponMapping(this);
 
-            if(brandName.toLowerCase().contains(query) || shortDescription.toLowerCase().contains(query)
-                    || longDescription.toLowerCase().contains(query) || requirementDescription.toLowerCase().contains(query)){
-                matchedObjects2.put(matchedObjects2.length(), currentCoupon);
-            }
-        }
-        return sortCouponsByCategory(matchedObjects2);
-    }
+		String coupons = HTTPRequest.executePostJSON(serviceMappings.getPath(), "", serviceMappings.getgenericHeader(), 0);
 
-    private JSONObject sortCouponsByCategory( JSONArray matchedObjects2 ){
-        Set<String> categoryIds = new HashSet<>();
-        JSONObject jsonObject = new JSONObject();
+		if (query.isEmpty()) {// == "") {
+			return coupons;
+		}
 
-        for (Object coupon: matchedObjects2){
-            JSONObject currentCoupon = (JSONObject) coupon;
-            categoryIds.add(currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.category));
-        }
+		JSONArray matchedObjects2 = new JSONArray();
+		return search(coupons, query, matchedObjects2).toString();
+	}
 
-        JSONArray retval = new JSONArray();
-        for(String id: categoryIds){
-            JSONObject formatting = new JSONObject();
-            JSONArray currentId = new JSONArray();
-            for(Object coupon: matchedObjects2){
-                JSONObject currentCoupon = (JSONObject) coupon;
-                String category = (currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.category));
-                if(category.equals(id)) {
-                    currentId.put(currentId.length(), currentCoupon);
-                }
-            }
-            formatting.put("Name", id);
-            formatting.put("Items", currentId);
-            retval.put(retval.length(), formatting);
-        }
-        jsonObject.put("CouponCategories", retval);
-        jsonObject.put("totalCoupons", totalCoupons(retval));
-        return jsonObject;
-    }
+	public Coupons() {
+		this.requestHeader = new WakefernHeader();
+	}
 
-    private int totalCoupons(JSONArray jsonArray){
-        int retval = 0;
-        for(Object category: jsonArray){
-            JSONObject currentCategory = (JSONObject) category;
-            retval += currentCategory.getJSONArray("Items").length();
-        }
-        return retval;
-    }
+	private JSONObject search(String coupons, String query, JSONArray matchedObjects2) {
+		query = query.toLowerCase();
+
+		JSONArray jsonArray = new JSONArray(coupons);
+
+		for (Object coupon : jsonArray) {
+			JSONObject currentCoupon = (JSONObject) coupon;
+
+			String brandName = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.brandName);
+			String shortDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.shortDescription);
+			String longDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.longDescription);
+			String requirementDescription = currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.requirementDescription);
+
+			if (brandName.toLowerCase().contains(query) 
+					|| shortDescription.toLowerCase().contains(query)
+					|| longDescription.toLowerCase().contains(query)
+					|| requirementDescription.toLowerCase().contains(query)) {
+				
+				matchedObjects2.put(matchedObjects2.length(), currentCoupon);
+			}
+		}
+		
+		return sortCouponsByCategory(matchedObjects2);
+	}
+
+	private JSONObject sortCouponsByCategory(JSONArray matchedObjects2) {
+		Set<String> categoryIds = new HashSet<>();
+		JSONObject jsonObject = new JSONObject();
+
+		for (Object coupon : matchedObjects2) {
+			JSONObject currentCoupon = (JSONObject) coupon;
+			categoryIds.add(currentCoupon.getString(WakefernApplicationConstants.Requests.Coupons.Search.category));
+		}
+
+		JSONArray retval = new JSONArray();
+		
+		for (String id : categoryIds) {
+			JSONObject formatting = new JSONObject();
+			JSONArray currentId = new JSONArray();
+			
+			for (Object coupon : matchedObjects2) {
+				JSONObject currentCoupon = (JSONObject) coupon;
+				String category = (currentCoupon
+						.getString(WakefernApplicationConstants.Requests.Coupons.Search.category));
+				if (category.equals(id)) {
+					currentId.put(currentId.length(), currentCoupon);
+				}
+			}
+			
+			formatting.put("Name", id);
+			formatting.put("Items", currentId);
+			retval.put(retval.length(), formatting);
+		}
+		
+		jsonObject.put("CouponCategories", retval);
+		jsonObject.put("totalCoupons", totalCoupons(retval));
+		
+		return jsonObject;
+	}
+
+	private int totalCoupons(JSONArray jsonArray) {
+		int retval = 0;
+		
+		for (Object category : jsonArray) {
+			JSONObject currentCategory = (JSONObject) category;
+			retval += currentCategory.getJSONArray("Items").length();
+		}
+		
+		return retval;
+	}
 }

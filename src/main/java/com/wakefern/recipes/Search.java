b9255d4de33e8fbe0116f2db0ddc4daf95f5b0e3
Bob <bob@labelinteractive.com>
2018-03-12 11:18:12 -0400

    Changed Accept header for recipe search, to one that results in recipe results coming back with category data.

--- src/main/java/com/wakefern/recipes/Search.java
+++ src/main/java/com/wakefern/recipes/Search.java
@@ -38,7 +38,7 @@ public class Search extends BaseService {
     		@HeaderParam(MWGApplicationConstants.Headers.Params.auth) String sessionToken
 	) throws Exception, IOException {
         		
-		this.requestHeader = new MWGHeader(MWGApplicationConstants.Headers.Recipes.recipes, MWGApplicationConstants.Headers.json, sessionToken);
+		this.requestHeader = new MWGHeader("application/vnd.mywebgrocer.wakefern-filter-facet-recipes+json", MWGApplicationConstants.Headers.json, sessionToken);
 		this.requestParams = new HashMap<String, String>();
 		this.queryParams   = new HashMap<String, String>();
 		

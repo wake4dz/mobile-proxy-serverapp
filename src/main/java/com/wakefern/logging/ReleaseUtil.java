e99eb6b53f89da95f212ec8e7d1b6a1c2ac01c5f
Danny Zheng <danny.zheng@wakefern.com>
2019-04-04 10:35:48 -0400

    add the new release status enhancement, and remove the old way of release code-based method

--- src/main/java/com/wakefern/logging/ReleaseUtil.java
+++ src/main/java/com/wakefern/logging/ReleaseUtil.java
@@ -1,29 +1,91 @@
 package com.wakefern.logging;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.log4j.Logger;
 
 public class ReleaseUtil {
-	private static List<Release> releaseList = null;
+	
+	private final static Logger logger = Logger.getLogger(ReleaseUtil.class);
+	
+	private static InputStream input = null;
+	
+	private static BufferedReader reader = null;
+	
+	private static List<String> releaseLines = new ArrayList<String>();
 	
 	static {
-	    releaseList = new ArrayList<Release>();
 
-		Release release3_x = new Release();
-		release3_x.setReleaseLevel("3.x");
-		release3_x.setReleaseDate("2018-07-13");
-		release3_x.setReleaseDescription("Starting release 4.0, the backend app would start tracking the release info");
-		releaseList.add(release3_x);
-		
-		Release release4_0 = new Release();
-		release4_0.setReleaseLevel("4.0");
-		release4_0.setReleaseDate("2018-10");
-		release4_0.setReleaseDescription("Rewrite the logging framework processes with log4j");
-		releaseList.add(release4_0);
+		try {
+
+			String fileName = "RELEASE_NOTE.properties";
+			input = ReleaseUtil.class.getClassLoader().getResourceAsStream(fileName);	
+			reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+			
+			if (reader == null) {
+				logger.error("Sorry, unable to find " + fileName);
+			}
+
+			String line = reader.readLine();
+			while (line != null) {
+				
+				if ((line == null) || (line.trim().length() == 0) || (line.trim().startsWith("#"))) {
+					//skip populating
+				} else {
+					releaseLines.add("The current " + line);
+				}
+				
+				// read next line
+				line = reader.readLine();
+			}
+			
+			
+			if ( ((releaseLines.size() / 3) < 1 ) || ((releaseLines.size() % 3) != 0 ) ) {
+
+				logger.error("releaseList.size() = " + releaseLines.size());
+				logger.error("Release info is not in the format of 3 lines");
+				
+				throw new Exception("Release info is not in the format of 3 lines");
+			}
+			
+			
+
+		} catch (Exception ex) {
+			releaseLines.clear();
+			
+			releaseLines.add("The current release_version = something went wrong...");
+			releaseLines.add("The current release_date    = something went wrong...");
+			releaseLines.add("The current release_branch  = something went wrong...");
+			
+			logger.error(LogUtil.getExceptionMessage(ex));
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					LogUtil.getExceptionMessage(e);
+				}
+			}
+			
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					LogUtil.getExceptionMessage(e);
+				}
+			}
+		}
 
 	}
 	
-	public static List<Release> getReleases() {	
-		return releaseList;
-	}
+	
+	public static List<String> getReleaseLines() {	
+		return releaseLines;
+	}	
+	
+
 }

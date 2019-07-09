399fe1ea26b7126676f18c99e3561dc92c708ac1
Kevin Jang <kevin.jang@wakefern.com>
2019-07-09 12:57:21 -0400

    feat: update search suggestions api request (#67)

--- src/main/java/com/wakefern/products/GetSuggestions.java
+++ src/main/java/com/wakefern/products/GetSuggestions.java
@@ -36,15 +36,7 @@ public class GetSuggestions extends BaseService {
     public Response getResponse(
     		@PathParam(MWGApplicationConstants.Requests.Params.Path.storeID) String storeID,
     		
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.excluded) String excludedProds,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.filters) String searchFilters,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.isMember) String isMember,
     		@QueryParam(MWGApplicationConstants.Requests.Params.Query.searchTerm) String searchTerm,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.skip) String skipCount,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.sortOrder) String sortOrder,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.searchBySound) String searchBySound,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.take) String takeCount,
-    		@QueryParam(MWGApplicationConstants.Requests.Params.Query.userID) String userID,
     		
     		@HeaderParam(MWGApplicationConstants.Headers.Params.accept) String accept,
     		@HeaderParam(MWGApplicationConstants.Headers.Params.contentType) String contentType,
@@ -53,34 +45,23 @@ public class GetSuggestions extends BaseService {
         try {
 			this.requestHeader = new MWGHeader(MWGApplicationConstants.Headers.json, MWGApplicationConstants.Headers.json, sessionToken);
 			this.requestParams = new HashMap<String, String>();
+			this.queryParams   = new HashMap<String, String>();
 			
 			// Build the Map of Request Path parameters
 			this.requestParams.put(MWGApplicationConstants.Requests.Params.Path.storeID, storeID);
+			this.queryParams.put(MWGApplicationConstants.Requests.Params.Query.searchTerm, searchTerm);
 					
             String jsonResponse = this.mwgRequest(BaseService.ReqType.GET, null, "com.wakefern.products.GetSuggestions");
-            
-			if(LogUtil.isUserTrackOn) {
-				if ((userID != null) && LogUtil.trackedUserIdsMap.containsKey(userID.trim())) {
-		        	String trackData = LogUtil.getRequestData("storeId", storeID, 
-		        			"isMember", isMember, "excludedProds", excludedProds, "searchFilters", searchFilters, 
-		        			"searchTerm", searchTerm, "skipCount", skipCount, "sortOrder", sortOrder,
-		        			"searchBySound", searchBySound, "takeCount", takeCount, "userID", userID, 
-		        			"sessionToken", sessionToken, "accept", accept, "contentType", contentType );
-					logger.info("Tracking data for " + userID + ": " + trackData + "; jsonResponse: " + jsonResponse);
-				}
-			}
 			
             return this.createValidResponse(jsonResponse);
-        
         } catch (Exception e) {
         	LogUtil.addErrorMaps(e, MwgErrorType.PRODUCTS_GET_SUGGESTIONS);
         	
         	String errorData = LogUtil.getRequestData("exceptionLocation", LogUtil.getRelevantStackTrace(e), 
-        			"storeId", storeID, 
-        			"isMember", isMember, "excludedProds", excludedProds, "searchFilters", searchFilters, 
-        			"searchTerm", searchTerm, "skipCount", skipCount, "sortOrder", sortOrder,
-        			"searchBySound", searchBySound, "takeCount", takeCount, "userID", userID, 
-        			"sessionToken", sessionToken, "accept", accept, "contentType", contentType );
+        			"storeId", storeID, "searchTerm", searchTerm, 
+        			"sessionToken", sessionToken, 
+        			"accept", accept, 
+        			"contentType", contentType );
 
     		logger.error(errorData + " - " + LogUtil.getExceptionMessage(e));
     		

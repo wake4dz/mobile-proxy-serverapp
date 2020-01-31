cb0349712f0c6a382122362f4aa7c5b76cf9fa85
Matt Miller <matthew.miller@wakefern.com>
2020-01-31 13:43:44 -0500

    update makeclean.sh

--- makeclean.sh
+++ makeclean.sh
@@ -2,4 +2,4 @@
 
 PWD=$(pwd)
 echo $PWD
-winpty docker run -it --rm --name maven-clean-install -v "$PWD":"/usr/src/app" -w /usr/src/app maven:3.2-jdk-7 mvn clean install
+winpty docker run -it --rm --name maven-build -v "/${PWD}":/usr/src/app -w //usr/src/app maven:3.6.2-jdk-8 mvn clean install

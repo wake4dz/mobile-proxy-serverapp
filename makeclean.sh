89a73def64530d5a07436b6228616bfd94477a0d
Matt Miller <matthew.miller@wakefern.com>
2020-02-04 14:03:57 -0500

    debug: add debugging commands to makeclean script

--- makeclean.sh
+++ makeclean.sh
@@ -2,4 +2,4 @@
 
 PWD=$(pwd)
 echo $PWD
-winpty docker run -it --rm --name maven-build -v "/${PWD}":/usr/src/app -w //usr/src/app maven:3.6.2-jdk-8 mvn clean install
+winpty docker run -it --rm --name maven-build -v "/${PWD}":/usr/src/app -w //usr/src/app maven:3.6.2-jdk-8 bash -c "pwd && ls -lah && mvn clean install"

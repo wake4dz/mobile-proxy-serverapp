1c478935bc7e91209de09a223001ff7dc28e9ab0
Danny Zheng <danny.zheng@wakefern.com>
2020-06-16 15:36:41 -0400

    refactor to load the json file into a static String object for quick access

--- src/main/java/com/wakefern/recipes/locai/GetRecipeCategories.java
+++ src/main/java/com/wakefern/recipes/locai/GetRecipeCategories.java
@@ -1,10 +1,5 @@
 package com.wakefern.recipes.locai;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,9 +8,8 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
-
 import com.wakefern.global.BaseService;
-import com.wakefern.global.VcapProcessor;
+
 import com.wakefern.logging.LogUtil;
 import com.wakefern.logging.MwgErrorType;
 import com.wakefern.mywebgrocer.MWGApplicationConstants;
@@ -39,32 +33,9 @@ public class GetRecipeCategories extends BaseService {
 	@Consumes(MWGApplicationConstants.Headers.generic)
 	@Path(WakefernApplicationConstants.RecipeLocai.Proxy.getRecipeCategories)
 	public Response getResponse() {
-		
-		InputStream is = null;
-		BufferedReader br = null;
-		
+
 		try {
-			String jsonFileName = null;
-			
-			//select which file to be sent based on recipe_service VCAP setting
-			if (VcapProcessor.getRecipeService().trim().equalsIgnoreCase("Staging")) {
-				jsonFileName = "recipe_qa.json";
-			} else {
-				jsonFileName = "recipe_prod.json";
-			}
-
-			is = getClass().getClassLoader().getResourceAsStream(jsonFileName);
-
-			br = new BufferedReader(new InputStreamReader(is));
-
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-			
-			JSONObject jsonObject = new JSONObject(sb.toString());
+			JSONObject jsonObject = new JSONObject(CategoryJsonFileCache.getJsonCache());
 			
 			return this.createValidResponse(jsonObject.toString());
 
@@ -74,21 +45,7 @@ public class GetRecipeCategories extends BaseService {
 			logger.error(errorData + " - " + LogUtil.getExceptionMessage(e));
 
 			return this.createErrorResponse(errorData, e);
-		} finally {
-			if (is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {
-				}
-			}
-			
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-				}
-			}
-		}
+		} 
 	}
 
 }

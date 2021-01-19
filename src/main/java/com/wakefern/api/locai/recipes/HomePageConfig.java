2b469155a91bb0040f7e45eb1d6eeba866194f5b
Danny Zheng <danny.zheng@wakefern.com>
2021-01-19 11:34:12 -0500

    solve the time zone issue

--- src/main/java/com/wakefern/api/locai/recipes/HomePageConfig.java
+++ src/main/java/com/wakefern/api/locai/recipes/HomePageConfig.java
@@ -1,11 +1,6 @@
 package com.wakefern.api.locai.recipes;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -73,7 +68,7 @@ public class HomePageConfig extends BaseService {
 	}
 
 	/**
-	 * Filter non-ShopRite layouts from the response body.
+	 * Filter non-ShopRite layouts and expired layouts from the response body.
 	 * 
 	 * @param response String
 	 * @return String
@@ -98,6 +93,7 @@ public class HomePageConfig extends BaseService {
 		for (int i = 0; i < numLayouts; i++) {
 			JSONObject layout = layouts.getJSONObject(i);
 
+			// filter out some invalid layouts/shelves
 			if ((isShopRiteBrand(layout)) && (isValidDateRange(layout))) {
 				filteredLayouts.put(layout);
 			}
@@ -139,7 +135,10 @@ public class HomePageConfig extends BaseService {
 		String shelfStart = null;
 		String shelfStop = null;
 		
-		Date currentDateTime = null;
+		Date currentServerDateTime = null;
+
+		Date startServerDateTime = null;
+		Date stopServerDateTime = null;
 		
 		try {
 			shelfTitle = layout.getString("title").trim();
@@ -148,49 +147,50 @@ public class HomePageConfig extends BaseService {
 	
 			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
 			
-			//TODO: using EST as the datetime reference. Dallas data center is 5-hr difference
-			sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+			sdf.setTimeZone(TimeZone.getTimeZone("America/New_York"));
 			
-			currentDateTime = new Date();
+			currentServerDateTime = new Date();
 			//String currentDateTimeString = sdf.format(currentDateTime);
-			
-			Date startDateTime = null;
-			Date stopDateTime = null;
 						
 			if ((shelfStart.length() == 0) && (shelfStop.length() == 0)) {
-				// good, no datetime restraint 
+				// good, no datetime restraint
 			} else if ((shelfStart.length() > 0) && (shelfStop.length() > 0)) {
-				startDateTime = sdf.parse(shelfStart);
-				stopDateTime = sdf.parse(shelfStop);
+				startServerDateTime = sdf.parse(shelfStart);
+				stopServerDateTime = sdf.parse(shelfStop);
 				
-				if ((currentDateTime.after(startDateTime)) && (currentDateTime.before(stopDateTime))) {
+				if ((currentServerDateTime.after(startServerDateTime)) && (currentServerDateTime.before(stopServerDateTime))) {
 					// good, within the date range
 				} else {
 					isValid = false;
 				}	
 			} else if ((shelfStart.length() > 0) && (shelfStop.length() == 0)) {
-				startDateTime = sdf.parse(shelfStart);
-				if (currentDateTime.after(startDateTime)) {
+				startServerDateTime = sdf.parse(shelfStart);
+				if (currentServerDateTime.after(startServerDateTime)) {
 					// good, shelf in effect without a stop datetime
 				} else {
 					isValid = false;
 				}
 			} else if ((shelfStart.length() ==0) && (shelfStop.length() > 0)) {
-				stopDateTime = sdf.parse(shelfStop);
-				if (currentDateTime.before(stopDateTime)) {
+				stopServerDateTime = sdf.parse(shelfStop);
+				if (currentServerDateTime.before(stopServerDateTime)) {
 					// good, shelf in effect without a start datetime
 				} else {
 					isValid = false;
 				}
 			} else {
 				// 4 different computations are already performed above, this "else" is more for warning message.
-				logger.warn("Unknown case for shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
+				logger.warn("Unknown case for shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", start datetime in server: " + startServerDateTime + ", "
+						+ "stop datetime: " + shelfStop + ", stop datetime in server: " + stopServerDateTime + ", current server datetime: " + currentServerDateTime + "]");
 			}
 			
-			logger.info("IsValid?: " + isValid + ", shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
+			// special note: current server time is 5-hr earlier than EST, but start/stop datetime are also 5-hrs earlier in server
+			// so, we are still comparing apple to apple in term of the timezone.
+			logger.info("IsValid?: " + isValid + ", shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", start datetime in server: " + startServerDateTime + ", "
+					+ "stop datetime: " + shelfStop + ", stop datetime in server: " + stopServerDateTime + ", current server datetime: " + currentServerDateTime + "]");
 
 		} catch (Exception e) {
-			logger.error("Shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
+			logger.error("Shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", start datetime in server: " + startServerDateTime + ", "
+					+ "stop datetime: " + shelfStop + ", stop datetime in server: " + stopServerDateTime + ", current server datetime: " + currentServerDateTime + "]");
 			logger.error(LogUtil.getRelevantStackTrace(e));
 			
 			// As documented at https://wakefern.atlassian.net/browse/DMAU-1473, we still want this shelf to be in UI even if there is an exception thrown
@@ -200,76 +200,4 @@ public class HomePageConfig extends BaseService {
 		return isValid;
 	}
 	
-	/*
-	 * only if the current date/time is btw a recipe's start datetime and stop
-	 * datetime If the start/end datetime value is null/empty, it means this recipe
-	 * is displayed all the time
-	 */
-	private static boolean validDateRange(JSONObject layout) {
-		boolean isValid = true;
-		
-		String shelfTitle = null;
-		String shelfStart = null;
-		String shelfStop = null;
-		
-		Date currentDateTime = null;
-		
-		try {
-			shelfTitle = layout.getString("title").trim();
-			shelfStart = layout.getString("start_time").trim();
-			shelfStop = layout.getString("stop_time").trim();
-	
-			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
-			
-			//TODO: using EST as the datetime reference. Dallas data center is 5-hr difference
-			sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-			
-			currentDateTime = new Date();
-			//String currentDateTimeString = sdf.format(currentDateTime);
-			
-			Date startDateTime = null;
-			Date stopDateTime = null;
-						
-			if ((shelfStart.length() == 0) && (shelfStop.length() == 0)) {
-				// good, no datetime restraint 
-			} else if ((shelfStart.length() > 0) && (shelfStop.length() > 0)) {
-				startDateTime = sdf.parse(shelfStart);
-				stopDateTime = sdf.parse(shelfStop);
-				
-				if ((currentDateTime.after(startDateTime)) && (currentDateTime.before(stopDateTime))) {
-					// good, within the date range
-				} else {
-					isValid = false;
-				}	
-			} else if ((shelfStart.length() > 0) && (shelfStop.length() == 0)) {
-				startDateTime = sdf.parse(shelfStart);
-				if (currentDateTime.after(startDateTime)) {
-					// good, shelf in effect without a stop datetime
-				} else {
-					isValid = false;
-				}
-			} else if ((shelfStart.length() ==0) && (shelfStop.length() > 0)) {
-				stopDateTime = sdf.parse(shelfStop);
-				if (currentDateTime.before(stopDateTime)) {
-					// good, shelf in effect without a start datetime
-				} else {
-					isValid = false;
-				}
-			} else {
-				// 4 different computations are already performed above, this "else" is more for warning message.
-				logger.warn("Unknown case for shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
-			}
-			
-			logger.info("IsValid?: " + isValid + ", shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
-
-		} catch (Exception e) {
-			logger.error("Shelf title: " + shelfTitle + " [start datetime: " + shelfStart+ ", stop datetime: " + shelfStop + ", current datetime: " + currentDateTime + "]");
-			logger.error(LogUtil.getRelevantStackTrace(e));
-			
-			// As documented at https://wakefern.atlassian.net/browse/DMAU-1473, we still want this shelf to be in UI even if there is an exception thrown
-			return true;
-		}
-		
-		return isValid;
-	}
 }

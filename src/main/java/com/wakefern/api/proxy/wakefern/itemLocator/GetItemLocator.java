87a8403f38852013255071974a7d46fb5843ab7e
wake4dz <danny.zheng@wakefern.com>
2023-05-30 15:45:49 -0400

    remove item locator v1 endpoint

--- src/main/java/com/wakefern/api/proxy/wakefern/itemLocator/GetItemLocator.java
+++ src/main/java/com/wakefern/api/proxy/wakefern/itemLocator/GetItemLocator.java
@@ -21,50 +21,13 @@ import java.util.Map;
 public class GetItemLocator extends BaseService {
 
 	private final static Logger logger = LogManager.getLogger(GetItemLocator.class);
-
-	@GET
-	@Produces(ApplicationConstants.Requests.Headers.MIMETypes.generic)
-	@Consumes(ApplicationConstants.Requests.Headers.MIMETypes.generic)
-	@Deprecated
-	@Path("/item/location/{storeId}/{upc}")
-	public Response getItem(@PathParam("storeId") String storeId, 
-			@PathParam("upc") String upc) { // note: upc's last digit of checksum of an UPC is already removed by caller
-		Map<String, String> wkfn = new HashMap<>();
-
-		String authToken = null;
-		
-		try {
-			String path = WakefernApplicationConstants.ItemLocator.baseURL
-					+ WakefernApplicationConstants.ItemLocator.locationPath + "/" + storeId + "/" + upc;
-
-			authToken = WakefernAuth.getAuthToken(EnvManager.getJwtPublicKey());
-			wkfn.put(ApplicationConstants.Requests.Headers.contentType, "application/json");
-			wkfn.put("Authentication", authToken);
-			wkfn.put(Requests.Headers.userAgent, ApplicationConstants.StringConstants.wakefernApplication);
-			// Call APIM Gateway to avoid any foreign IP addresses
-			wkfn.put(WakefernApplicationConstants.APIM.sub_key_header, EnvManager.getMobilePassThruApiKeyProd());
-
-			logger.trace("URL path: " + path);
-
-			return this.createValidResponse(HTTPRequest.executeGet(path, wkfn, EnvManager.getApiMediumTimeout()));
-		} catch (Exception e) {
-
-			String errorData = parseAndLogException(logger, e, 
-					ApplicationConstants.Requests.Headers.contentType, "application/json",
-					"upc", upc,
-					"Authentication", authToken,
-					WakefernApplicationConstants.APIM.sub_key_header, EnvManager.getMobilePassThruApiKeyProd());
-
-			return this.createErrorResponse(errorData, e);
-		}
-	}
 	
 	@GET
 	@Produces(ApplicationConstants.Requests.Headers.MIMETypes.generic)
 	@Consumes(ApplicationConstants.Requests.Headers.MIMETypes.generic)
 	@Path("/item/location/v2/{storeId}/{upc}")
 	public Response getItemV2(@PathParam("storeId") String storeId,
-							@PathParam("upc") String upc) {
+							@PathParam("upc") String upc) { // note: upc's last digit of checksum of an UPC is already removed by caller
 		Map<String, String> wkfn = new HashMap<>();
 		ItemLocatorDto cached = ItemLocatorCache.getInstance().get(storeId, upc);
 

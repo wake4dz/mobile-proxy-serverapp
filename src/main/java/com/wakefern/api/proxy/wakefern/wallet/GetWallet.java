433b8a3abcd867ca58823f26cdb04ef80eb046b5
Danny Zheng <danny.zheng@wakefern.com>
2023-06-20 14:11:38 -0400

    Feat/wallet/updated api (#285)
    * updates from Sherry
    
    * update release #
    
    * update wallet impl

--- src/main/java/com/wakefern/api/proxy/wakefern/wallet/GetWallet.java
+++ src/main/java/com/wakefern/api/proxy/wakefern/wallet/GetWallet.java
@@ -18,20 +18,15 @@ import com.wakefern.global.ApplicationConstants;
 import com.wakefern.global.ApplicationConstants.Requests;
 import com.wakefern.global.BaseService;
 import com.wakefern.global.EnvManager;
-
 import com.wakefern.request.HTTPRequest;
 import com.wakefern.wakefern.WakefernApplicationConstants;
 /**  
-* A proxy API to access a home-grown Wallet API
+* A proxy API to access a home-grown Wallet API by Sherry Shi
 *   
 * @author  Danny Zheng
 *
 */ 
 
-/*
- * 2022-08-01
- * TODO: re-test after Sherry's back-end is ready
- */
 @Path(ApplicationConstants.Requests.Proxy + WakefernApplicationConstants.Wallet.Proxy.Path)
 public class GetWallet extends BaseService {
 
@@ -42,22 +37,24 @@ public class GetWallet extends BaseService {
     @Consumes(ApplicationConstants.Requests.Headers.MIMETypes.generic)
     @Path(WakefernApplicationConstants.Wallet.Proxy.GetWallet)
     public Response getResponse(
-    		@PathParam(WakefernApplicationConstants.Wallet.RequestParamsPath.Device) String device, // android or iOs
+    		// Only apply for ShopRite and TheFreshGrocer banners as of 7/2023
+    		@PathParam(WakefernApplicationConstants.Wallet.RequestParamsPath.Banner) String banner,
+    		// device value would be 'andr' or 'ios'
+    		@PathParam(WakefernApplicationConstants.Wallet.RequestParamsPath.Device) String device, 
     		@PathParam(WakefernApplicationConstants.Wallet.RequestParamsPath.CustomerId) String customerId,
-    		@PathParam(WakefernApplicationConstants.Wallet.RequestParamsPath.SessionToken) String sessionToken,
     		@HeaderParam(WakefernApplicationConstants.Wallet.HeadersParams.ContentType) String contentType) {
 
         Map<String, String> headers = new HashMap<>();
-        
-        try {
-        	String path =  EnvManager.getTargetWalletServiceEndpoint()
+       
+        try {     	
+        	String path =  EnvManager.getTargetWalletServiceEndpoint() 
+        			+ "/" + banner
         			+ "/" + device 
-        			+ "/" + customerId
-        			+ "/" + sessionToken;
-
-            headers.put("Content-Type", contentType);
-            headers.put("Authorization", EnvManager.getTargetWalletAuthorizationKey() );
-            headers.put(Requests.Headers.userAgent, ApplicationConstants.StringConstants.wakefernApplication);
+        			+ "/" + customerId;
+        	
+            headers.put(ApplicationConstants.Requests.Headers.contentType, contentType);
+            headers.put(ApplicationConstants.Requests.Headers.xApiKey, EnvManager.getTargetWalletAuthorizationKey() );
+            headers.put(ApplicationConstants.Requests.Headers.userAgent, ApplicationConstants.StringConstants.wakefernApplication);
 
             //note: first-time call for each user would take some time. After cache in the upstream system, calls are much faster.
             return this.createValidResponse(HTTPRequest.executeGet(path, headers, EnvManager.getApiMediumTimeout()));
@@ -65,11 +62,11 @@ public class GetWallet extends BaseService {
         } catch (Exception e) {
 
 			String errorData = parseAndLogException(logger, e, 
+					WakefernApplicationConstants.Wallet.RequestParamsPath.Banner, banner,
 					WakefernApplicationConstants.Wallet.HeadersParams.ContentType, contentType,
-					WakefernApplicationConstants.Wallet.RequestParamsPath.SessionToken, sessionToken,
 					WakefernApplicationConstants.Wallet.RequestParamsPath.Device, device,
 					WakefernApplicationConstants.Wallet.RequestParamsPath.CustomerId, customerId,
-					"Authorization", EnvManager.getTargetWalletAuthorizationKey());
+					ApplicationConstants.Requests.Headers.xApiKey, EnvManager.getTargetWalletAuthorizationKey());
 			
             return this.createErrorResponse(errorData, e);
         }
